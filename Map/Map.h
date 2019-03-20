8d899c3010c23d1472b4ab8816f1235db5296ff5
darienmiller88 <32966645+darienmiller88@users.noreply.github.com>
2019-03-20 19:19:34 -0400

    Update Map.h

--- Map/Map.h
+++ Map/Map.h
@@ -8,13 +8,6 @@ using Entry = std::pair<std::string, int>;
 
 //Map class that will map strings to int for simplicity.
 class Map{
-	
-	struct Node {
-		std::string key;
-		int value;
-		std::unique_ptr<Node> left;
-		std::unique_ptr<Node> right;
-	};
 
 	public:
 		Map();
@@ -27,47 +20,14 @@ class Map{
 		int &operator [] (const std::string &key);
 		friend std::ostream &operator << (std::ostream &os, const Map &m);
 
-		class iterator {
-			public:
-				iterator(Node *data = nullptr) : data(data){
-
-				}
-
-				iterator(const iterator &rhs) : data(rhs.data) {
-
-				}
-
-				iterator &operator =(const iterator &rhs) {
-
-				}
-
-				iterator &operator++() {
-
-				}
-
-				iterator &operator++(int) {
-
-				}
-
-				Node &operator *() {
-					return *data;
-				}
-
-
-
-			
-			private:
-				Node *data;
-		};
-
-		iterator begin() {
-			return iterator(head.get());
-		}
-
-		iterator end() {
-			return iterator();
-		}*/
+		
 	private:
+		struct Node {
+			std::string key;
+			int value;
+			std::unique_ptr<Node> left;
+			std::unique_ptr<Node> right;
+		};
 		Node *newNode;//this pointer will keep track of where each new node is.
 		void insertIntoTree(std::unique_ptr<Node> &head, const Entry &entry);
 		static void printInOrder(std::ostream &os, Node *root);
